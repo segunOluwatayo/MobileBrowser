6ad20cddc7e843ab79cf83f6f27d87f079a9b759
“Segun <Oluwatayoo08@gmail.com>
2024-12-26 17:08:02 +0000

    Updated the MainActivity with GeckoView navigation controls

--- app/src/main/java/com/example/mobilebrowser/MainActivity.kt
+++ app/src/main/java/com/example/mobilebrowser/MainActivity.kt
@@ -8,22 +8,36 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.example.mobilebrowser.ui.composables.BrowserContent
-import com.example.mobilebrowser.ui.theme.MobileBrowserTheme  // Updated import
+import com.example.mobilebrowser.ui.theme.MobileBrowserTheme
+import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoSession
 
 class MainActivity : ComponentActivity() {
+    private lateinit var geckoSession: GeckoSession
+    private lateinit var geckoRuntime: GeckoRuntime
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MobileBrowserTheme {  // Updated theme name
-                val currentUrl by remember { mutableStateOf("https://www.mozilla.org") }
+            MobileBrowserTheme {
+                var currentUrl by remember { mutableStateOf("https://www.mozilla.org") }
                 val canGoBack by remember { mutableStateOf(false) }
                 val canGoForward by remember { mutableStateOf(false) }
 
                 BrowserContent(
-                    onNavigate = { url -> /* Will implement in next step */ },
-                    onBack = { /* Will implement in next step */ },
-                    onForward = { /* Will implement in next step */ },
-                    onReload = { /* Will implement in next step */ },
+                    onNavigate = { url ->
+                        currentUrl = url
+                    },
+                    onBack = {
+                        geckoSession.goBack()
+                    },
+                    onForward = {
+                        geckoSession.goForward()
+                    },
+                    onReload = {
+                        geckoSession.reload()
+                    },
                     canGoBack = canGoBack,
                     canGoForward = canGoForward,
                     currentUrl = currentUrl
