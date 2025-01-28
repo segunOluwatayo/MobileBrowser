a41259c01f3b4989d442ff39cff11ea20875c4b4
“Segun <Oluwatayoo08@gmail.com>
2025-01-28 11:27:03 +0000

    Slight change with the gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.mobilebrowser"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mobilebrowser"
